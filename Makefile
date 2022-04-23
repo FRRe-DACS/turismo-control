4fe832de0c0e30ece263b7689f052ada207688b9
jorgevillaverde <jorge.villaverde@toucansoft.io>
2022-04-23 11:27:13 -0300

    Initial Commit

---
+++ Makefile
@@ -0,0 +1,20 @@
+SERVER_OUT=bin/turismo
+BANNER=banner.txt
+BANNER_OUT=bin/banner.txt
+PKG := "github.com/FRRe-DACS/turismo-control"
+DOCKER_TAG=frredacs/turismo-control:1.0
+
+.PHONY: docker
+
+dep: ## Get the dependencies
+	@go get -v -d ./...
+copy_banner:
+
+server: dep 
+	@go build -i -v -o $(SERVER_OUT) && cp $(BANNER) $(BANNER_OUT)
+
+docker:
+	@docker build -t $(DOCKER_TAG) .
+
+clean: ## Remove previous builds
+	@rm $(SERVER_OUT) $(BANNER_OUT)
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+
+	_ "github.com/dimiro1/banner/autoload"
+)
+
+// operacion representa una operación de turismo
+type operacion struct {
+	ID            int       `json:"id"`
+	CUIT          int64     `json:"cuit"`
+	FechaCreacion time.Time `json:"fecha_creacion"`
+	FechaInicio   time.Time `json:"fecha_incio"`
+	FechaFin      time.Time `json:"fecha_fin"`
+	Precio        float64   `json:"precio"`
+	Aprobada      bool      `json:"aprobada"`
+}
+
+type allOperaciones []operacion
+
+var operaciones = allOperaciones{}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+type operacion_request struct {
+	CUIT        int64     `json:"cuit"`
+	FechaInicio time.Time `json:"fecha_incio"`
+	FechaFin    time.Time `json:"fecha_fin"`
+	Precio      float64   `json:"precio"`
+}
+
+func createOperacion(w http.ResponseWriter, r *http.Request) {
+	var operacionReq operacion_request
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Por favor ingrese bien los datos de la operación")
+	}
+
+	json.Unmarshal(reqBody, &operacionReq)
+
+	log.Print("Creando Operacion: ", operacionReq)
+	enableCors(&w)
+
+	var randFloat32 = rand.Float32()
+	var approbado bool = (randFloat32 > 0.5)
+
+	var newOperacion = operacion{
+		ID:            len(operaciones),
+		CUIT:          operacionReq.CUIT,
+		FechaCreacion: time.Now(),
+		FechaInicio:   operacionReq.FechaInicio,
+		FechaFin:      operacionReq.FechaFin,
+		Precio:        operacionReq.Precio,
+		Aprobada:      approbado,
+	}
+
+	operaciones = append(operaciones, newOperacion)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newOperacion)
+}
+
+func main() {
+	log.Print("Starting Turismo Control RESTful API!")
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/operacion", createOperacion).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
